e113ead40fa1cb4a9770dc48430ca75a99b070b3
admin <admin@172.16.213.176>
2024-02-13 11:53:54 +0700

    updated

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Katalon-TaskMobile-NovaAdityaUtami
+
+Melakukan pengujian pada mobile solodroid e-commerce dengan pegujian sebagai berikut :
+
+Test suites - Checkout
+1. Checkout barang dengan pilihan yes
+- pilih barang salah satu barang
+- klik add to chart
+- input number order
+- klik checkout
+- Input pilihan pada checkout
+- pilih proses checkout yes
+- barang berhasil di checkout
+
+
+2. Checkout barang dengan pilihan no
+- pilih barang salah satu barang
+- klik add to chart
+- input number order
+- klik checkout
+- Input pilihan pada checkout
+- pilih proses checkout no
+- barang gagal di checkout
+
+Test suites - Search and category
+
+1. Search
+- Klik search pada halaman utama
+- Cari salah satu barang
+- Barang berhasil di temukan
+
+2. Category
+- Klik menu categori
+- Pilih salah satu category
+- Barang berdasarkan pilihan category berhasil muncul
---
+++ Test Suites/Search and Category.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Search and Category</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>655fa033-055d-4e4b-86f1-fa6c6e5f3fa1</testSuiteGuid>
+   <testCaseLink>
+      <guid>866b3f4f-a8d1-4203-bc24-e2721fd91c24</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/search</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8a29fb9b-5698-4111-80ca-b8c692bf9328</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Categori</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
